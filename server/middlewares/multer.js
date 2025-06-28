c8f0a782405cb41a3edc03488400e92175c56681
manas_pachauri <manaspachauri31@gmail.com>
2025-06-28 16:46:07 +0530

    updated

--- server/middlewares/multer.js
+++ server/middlewares/multer.js
@@ -3,9 +3,7 @@ import multer from 'multer';
 // creating multer middleware for parsing form-data
 
 const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, 'uploads/');
-  },
+
   filename: function (re,file,callback) {
     callback(null, `${Date.now()}_${file.originalname}`);
   },
